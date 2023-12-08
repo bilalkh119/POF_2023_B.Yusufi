bdde605ed0debd92aa44ee6bad4d675dfb5f802c
Bilal Khan Yusufi <153234889+bilalkh119@users.noreply.github.com>
2023-12-08 12:05:03 +0100

    Update readme.cmd

--- Mesh/readme.cmd
+++ Mesh/readme.cmd
@@ -1,1 +1,4 @@
-
+The o-grid mesh file for one of the cases can be found in this folder.
+There are a total of 14 files, compressed under two zip folders: Mesh001 and Mesh002. 
+Hence, Mesh001 and Mesh002 are needed to be extracted before using them. 
+The extracted files can be directly accessed in ICEM-CFD, using the commands: File > Open Project > Beta_loop
